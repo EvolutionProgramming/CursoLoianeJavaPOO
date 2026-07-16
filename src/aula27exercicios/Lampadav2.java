24277d6b59784108b349e56c50318a98f0ce775b
EvolutionProgramming <262878345+EvolutionProgramming@users.noreply.github.com>
2026-07-16 17:15:32 -0300

    Refatorei o código pois aprendi que é importante a separação de responsabilidades entre a Classe e neste exemplo o Menu do objeto lampada

--- src/aula27exercicios/Lampadav2.java
+++ src/aula27exercicios/Lampadav2.java
@@ -7,32 +7,15 @@ public class Lampadav2 {
     double preco;
     int tensao;
     int potencia;
+    boolean estado;
 
-    boolean validadorComando(int comando) {
-        return comando != 0;
-    }
-
-    void informarComandoInvalido() {
-        System.out.println("Comando inválido! Digite 1 para Ligar ou 2 para Desligar");
-        System.out.println();
-        System.out.println("--------------------------------");
-    }
 
     void ligarLampada() {
-        System.out.println("Lampada LIGADA com sucesso!");
-        System.out.println();
-        System.out.println("--------------------------------");
+        estado = true;
     }
 
     void desligarLampada() {
-        System.out.println("Lampada DESLIGADA com sucesso!");
-        System.out.println();
-        System.out.println("--------------------------------");
-
+        estado = false;
     }
 
-    void sairLampada() {
-        System.out.println("Saindo do Sistema...");
-        System.exit(0);
-    }
 }
--- src/aula27exercicios/Lampadav2Teste.java
+++ src/aula27exercicios/Lampadav2Teste.java
@@ -8,28 +8,53 @@ public class Lampadav2Teste {
         Lampadav2 lampada = new Lampadav2();
 
         int comando;
-        boolean validador;
+        boolean validador = true;
 
         while (validador) {
-            System.out.println("Digite 1 para Ligar ou 2 para Desligar (Digite 0 para sair)");
+            System.out.println("Digite 1 para Ligar ou 2 para Desligar (Digite qualquqer valor para sair do menu)");
             comando = sc.nextInt();
-            validador = lampada.validadorComando(comando);
+            sc.nextLine();
 
-            switch (comando) {
-                case 1:
-                    lampada.ligarLampada();
-                    break;
-                case 2:
-                    lampada.desligarLampada();
-                    break;
-                case 0:
-                    lampada.sairLampada();
-                    break;
-                default:
-                    lampada.informarComandoInvalido();
-                    continue;
+            validador = validadorComando(comando);
+            Lampadav2Teste.exibirResultadoComando(validador, comando, lampada);
 
+        }
+        sc.close();
+    }
+
+    static boolean validadorComando(int comando) {
+        return comando > 0 && comando < 3;
+    }
+
+    static void exibirResultadoComando(boolean validador, int comando, Lampadav2 lampada) {
+        if (validador) {
+            if (comando == 1 && lampada.estado) {
+                System.out.println("------------------------------");
+                System.out.println("Lampada já está ligada!!!");
+                System.out.println("------------------------------");
+                System.out.println();
+            } else if (comando == 2 && !lampada.estado) {
+                System.out.println("------------------------------");
+                System.out.println("Lampada já está desligada!!!");
+                System.out.println("------------------------------");
+                System.out.println();
+
+            } else if (comando == 1) {
+                System.out.println("------------------------------");
+                System.out.println("Você ligou a lâmpada!");
+                System.out.println("------------------------------");
+                System.out.println();
+                lampada.ligarLampada();
+
+            } else if (comando == 2) {
+                System.out.println("------------------------------");
+                System.out.println("Você desligou a lâmpada!");
+                System.out.println("------------------------------");
+                System.out.println();
+                lampada.desligarLampada();
             }
+        } else {
+            System.out.println("Finalizando sistema...");
         }
     }
 }

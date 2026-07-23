aaf7a8ba09af94f36d95280c97093a3c396b6e14
EvolutionProgramming <262878345+EvolutionProgramming@users.noreply.github.com>
2026-07-23 17:22:35 -0300

    Aplicando o this e melhorando o código com base em boas práticas

--- src/aula30/Carrov6.java
+++ src/aula30/Carrov6.java
@@ -8,27 +8,15 @@ public class Carrov6 {
     double consumoCombustivel;
 
     Carrov6() {
-        System.out.println("Classe Carro foi instaciada (Via construtor)");
-        //Se haver a atribuição na classe, deve se colocar sempre no construtor
-        //Pode haver varios construtores se eles forem diferentes
+
     }
 
     Carrov6(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
         this.marca = marca;
-        this.modelo = modelo;
         this.numPassageiros = numPassageiros;
         this.capCombustivel = capCombustivel;
         this.consumoCombustivel = consumoCombustivel;
     }
-
-    void exibirMarcaModelo() {
-        System.out.println("Carro " + this.marca + " " + this.modelo);
-    }
-
-    void exibirNumPassageiros() {
-        System.out.println("Suporta " + this.numPassageiros + " pessoas");
-    }
-
 
     double obterAutonomia() {
         return this.capCombustivel * this.consumoCombustivel;
@@ -36,14 +24,21 @@ public class Carrov6 {
 
     double calcularCombustivel(double km) {
 
-        double qtdCombustivel = km / this.consumoCombustivel;
-
-        return qtdCombustivel;
+        return km / this.consumoCombustivel;
 
     }
 
     void exibirAutonomia() {
         double autonomia = obterAutonomia();
         System.out.println("Autonomia de: " + autonomia + "km");
+    }
+
+
+    public void exibirMarcaModelo() {
+        System.out.println("Carro " + this.marca + " " + this.modelo);
+    }
+
+    public void exibirNumPassageiros() {
+        System.out.println("Suporta " + this.numPassageiros + " pessoas");
     }
 }
--- src/aula30/TesteCarrov6.java
+++ src/aula30/TesteCarrov6.java
@@ -1,19 +1,11 @@
 package aula30;
 
-import aula29.Carrov5;
-
 import java.util.Scanner;
 
 public class TesteCarrov6 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        Carrov6 popular = new Carrov6();
-
-        popular.marca = "Ford";
-        popular.modelo = "Fiesta";
-        popular.numPassageiros = 5;
-        popular.capCombustivel = 47.0;
-        popular.consumoCombustivel = 12.0;
+        Carrov6 popular = new Carrov6("Ford", "Fiesta", 5, 47.0, 12.0);
 
         popular.exibirMarcaModelo();
         popular.exibirNumPassageiros();
@@ -29,10 +21,5 @@ public class TesteCarrov6 {
         if (Character.toLowerCase(querObter) == 's') {
             popular.exibirAutonomia();
         }
-
-        // ferrari inventada (numeros irreais)
-        Carrov6 popular2 = new Carrov6("Ferrari", "Roma", 10, 66, 15);
-        System.out.println(popular2.marca + "\n" + popular2.modelo + "\n" + popular2.numPassageiros + "\n" + popular2.capCombustivel + "\n" + popular2.consumoCombustivel);
-        System.out.println();
     }
 }

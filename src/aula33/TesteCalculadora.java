7536f4f5ffc74c2acfa1e06021561c6c800c7256
EvolutionProgramming <262878345+EvolutionProgramming@users.noreply.github.com>
2026-07-24 19:48:31 -0300

    Estudei sobre overload e suas regras para ser bem aplicado

---
+++ src/aula33/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package aula33;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        MinhaCalculadora calculadora = new MinhaCalculadora();
+
+        //inteiro
+        calculadora.soma(1,2);
+        //double
+        calculadora.soma(1.0,2.0);
+
+    }
+}

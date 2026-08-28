0e8901437bb6d878c3d0d7fbd458a37db9643bc3
EvolutionProgramming <262878345+EvolutionProgramming@users.noreply.github.com>
2026-08-28 19:14:03 -0300

    Aplicando o final, excluindo os getters dos atributos e chamando os atributos direto nos métodos

--- src/tema4_variaveis_e_metodos_estaticos/aula34exercicios/ConversaoDeUnidadesDeTempo.java
+++ src/tema4_variaveis_e_metodos_estaticos/aula34exercicios/ConversaoDeUnidadesDeTempo.java
@@ -4,40 +4,40 @@ public class ConversaoDeUnidadesDeTempo {
     private static double resultado;
 
     // 1 minuto convertido para minutos
-    private static double minutoPraSegundos= 60;
+    private static final double MINUTO_PARA_SEGUNDOS= 60;
     // 1 hora convertido para minutos
-    private static double horaPraMinutos = 60;
+    private static final double HORA_PARA_MINUTOS = 60;
     // 1 dia convertido para horas
-    private static double diaPraHoras = 24;
+    private static final double DIA_PARA_HORAS = 24;
     // 1 semana convertido para dias
-    private static double semanaPraDias = 7;
+    private static final double SEMANA_PARA_DIAS = 7;
     // 1 mês convertido para dias
-    private static double mesPraDias = 30;
+    private static final double MES_PARA_DIAS = 30;
     // 1 ano convertido para dias
-    private static double anoPraDias = 365.25;
+    private static final double ANO_PARA_DIAS = 365.25;
 
     public static double converterMinutoParaSegundos(double numero) {
-        return numero * getMinutoPraSegundos();
+        return numero * MINUTO_PARA_SEGUNDOS;
     }
 
     public static double converterHoraParaMinutos(double numero) {
-        return numero * getHoraPraMinutos();
+        return numero * HORA_PARA_MINUTOS;
     }
 
     public static double converterDiaParaHoras(double numero) {
-        return numero * getDiaPraHoras();
+        return numero * DIA_PARA_HORAS;
     }
 
     public static double converterSemanaParaDias(double numero) {
-        return numero * getSemanaPraDias();
+        return numero * SEMANA_PARA_DIAS;
     }
 
     public static double converterMesParaDias(double numero) {
-        return numero * getMesPraDias();
+        return numero * MES_PARA_DIAS;
     }
 
     public static double converterAnoParaDias(double numero) {
-        return numero * getAnoPraDias();
+        return numero * ANO_PARA_DIAS;
     }
 
     public static double getResultado() {
@@ -46,29 +46,5 @@ public class ConversaoDeUnidadesDeTempo {
 
     public static void setResultado(double resultado_) {
         resultado = resultado_;
-    }
-
-    public static double getMinutoPraSegundos() {
-        return minutoPraSegundos;
-    }
-
-    public static double getHoraPraMinutos() {
-        return horaPraMinutos;
-    }
-
-    public static double getDiaPraHoras() {
-        return diaPraHoras;
-    }
-
-    public static double getSemanaPraDias() {
-        return semanaPraDias;
-    }
-
-    public static double getMesPraDias() {
-        return mesPraDias;
-    }
-
-    public static double getAnoPraDias() {
-        return anoPraDias;
     }
 }
